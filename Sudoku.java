422116379b6119b3d048a3bdd546353d30040f3a
Jason Wang <71799030+jason-j-wang@users.noreply.github.com>
2021-02-08 13:43:24 -0800

    Update Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -7,6 +7,7 @@ package sudoku;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.LayoutManager;
 
@@ -26,6 +27,9 @@ public class Sudoku extends JFrame {
     private static int ROW = 3;
     private static int COL = 3;
     private Button[][] buttons;
+    private Button currentButton;
+    private boolean completed;
+    private JLabel message;
     
     
     private int[][] board = {
@@ -40,22 +44,35 @@ public class Sudoku extends JFrame {
         {7, 9, 1, 4, 8, 5, 6, 2, 3}
     };
     
-    private boolean[][] filled = {
+    /*private boolean[][] filled = {
         {true, true, true, true, true, true, false, true, false},
         {true, true, false, true, false, true, true, false, true},
         {false, true, true, true, false, true, true, false, true},
         {true, true, false, false, false, true, false, false, false},
         {true, false, true, false, false, true, false, true, false},
-        {true, false, false, true, false, true, true, false, false},
+        {true, false, false, true, true, true, true, false, false},
         {true, false, false, false, true, true, true, true, true},
         {true, false, true, true, false, false, true, true, true},
         {false, true, false, false, true, true, true, false, false}
+    }; */
+    
+    private boolean[][] filled = {
+        {true, true, true, true, true, true, true, true, true},
+        {true, true, true, true, true, true, true, true, true},
+        {true, true, true, true, true, true, true, true, true},
+        {true, true, true, true, true, true, true, true, true},
+        {true, true, true, true, true, true, true, true, true},
+        {true, true, true, true, true, true, true, true, true},
+        {true, true, true, true, true, true, true, true, true},
+        {true, true, true, true, true, true, true, true, true},
+        {true, true, true, true, true, true, true, true, false}
     };
     
     public Sudoku(){
         super("Sudoku");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+        completed = false;
         
         JPanel masterPanel = new JPanel();
         masterPanel.setLayout(new GridLayout(ROW, COL));
@@ -63,39 +80,85 @@ public class Sudoku extends JFrame {
         Border blackline = BorderFactory.createLineBorder(Color.black);
         
         buttons = new Button[ROW * ROW][COL * COL];
-        for (int i = 0; i < ROW; i++){
-            for (int j = 0; j < COL; j++){
+        for (int outerRow = 0; outerRow < ROW; outerRow++){
+            for (int outerCol = 0; outerCol < COL; outerCol++){
                 JPanel gridPanel = new JPanel();
                 gridPanel.setLayout(new GridLayout(ROW, COL));
                 gridPanel.setBorder(blackline);
                 
-                for (int r = 0; r < ROW; r++){
-                    for (int c = 0; c < COL; c++){
-                        Button b = new Button();
+                for (int innerRow = 0; innerRow < ROW; innerRow++){
+                    for (int innerCol = 0; innerCol < COL; innerCol++){
+                        int num = board[outerRow * 3 + innerRow][outerCol * 3 + innerCol];
+                        boolean visible = filled[outerRow * 3 + innerRow][outerCol * 3 + innerCol];
+                        Button b = new Button(num, visible);
                         gridPanel.add(b);
-                        //buttons[i * r][j * c] = b; fix
+                        buttons[outerRow * 3 + innerRow][outerCol * 3 + innerCol] = b;
+                        
+                        b.addActionListener(new ClickHandler(this));
+                        if (!visible){
+                            b.addKeyListener(new KeyboardInput(this));
+                        }
                         
                     }
                 }
                 masterPanel.add(gridPanel);
             }
         }
-        
         masterPanel.setBorder(blackline);  
         
-        this.add(messagePanel);
-        this.add(masterPanel);
+        message = new JLabel();
+        message.setText("Solve the Sudoku");
+        messagePanel.add(message);
+
+        
+        this.add(messagePanel, BorderLayout.NORTH);
+        this.add(masterPanel, BorderLayout.SOUTH);
         this.pack();
         
     }
+    
+    public void setCurrentButton(Button b){
+        if (currentButton != null){
+            currentButton.deselect();
+        }
+        currentButton = b;
+        b.select();
+    }
+    
+    public Button getCurrentButton(){
+        return currentButton;
+    }
+    
+    public boolean isCompleted(){
+        boolean win = true;
+        for (int i = 0; i < COL * COL; i++){
+            for (int j = 0; j < ROW * ROW; j++){
+                Button b = buttons[i][j];
+                if (b.getValue() != b.getPlayerValue()){
+                    win = false;
+                    
+                }
+            }
+        }
+        if (win){
+            completed = true;
+        }
+
+        return win;
+    }
+    
+    public void checkIfWon(){
+        if (isCompleted()){
+            message.setText("Victory Royale!");
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
        
-        
         Sudoku game = new Sudoku();
-        
            
         }
     }
