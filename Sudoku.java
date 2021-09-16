e14078f95ebe4f8bc0e974bc384bd0a46c2d9692
Jason Wang <71799030+jason-j-wang@users.noreply.github.com>
2021-09-15 23:21:11 -0700

    Update Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sudoku;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
@@ -153,14 +147,6 @@ public class Sudoku extends JFrame {
         }
     }
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-       
-        Sudoku game = new Sudoku();
-           
-        }
     }
-    
+
 
